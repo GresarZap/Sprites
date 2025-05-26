7af1586062309b5a6574f5a86af54070534522fa
sergio <kuroigresar.shiro@gmail.com>
2025-05-26 18:36:08 -0400

    Cambiando Lampara por Sprites para el correcto funcionamiento de la ruta

--- utils/load_file.js
+++ utils/load_file.js
@@ -18,7 +18,7 @@ export async function loadFile(path) {
 
 function buildUrl(path) {
     let isLocal = window.location.hostname === '127.0.0.1';
-    const baseUrl = window.location.origin + (isLocal?'':'/Lampara/');
+    const baseUrl = window.location.origin + (isLocal?'':'/Sprites/');
     console.log(`Base URL: ${baseUrl}, Path: ${path}`);
     return baseUrl + path;
 }
